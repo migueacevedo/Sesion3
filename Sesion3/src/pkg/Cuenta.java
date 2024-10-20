8b6d1e7c343a427c0ed30e506adc6197b60eaf6b
Miguel Angel <Miguel Angel@DESKTOP-8B9CCP6>
2024-10-20 22:41:50 +0200

    commit clase cuenta actualizada

--- Sesion3/src/pkg/Cuenta.java
+++ Sesion3/src/pkg/Cuenta.java
@@ -1,31 +1,72 @@
 package pkg;
 
+import pkg.Movimiento.Signo;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cuenta {
-	Double saldo;
-	
-	public Cuenta(Double saldo)
-	{
-		this.saldo=saldo;
-	}
-	
-	public Double getSald() {
-		return saldo;
-	}
-	
-	public void setSaldo(Double saldo) {
-		this.saldo=saldo;
+	private double saldo;
+	private String numero;
+	private String titular;
+	private List<Movimiento> movs;
+
+	public Cuenta(String numeroCuenta, String titular, double saldo) {
+		super();
+		this.saldo = saldo;
+		this.numero = numeroCuenta;
+		this.titular = titular;
+		this.movs = new ArrayList<Movimiento>();
 	}
 
-	public Double getSaldo() {
-		return saldo;
+	public void ingresar(double cantidad) {
+		this.saldo += cantidad;
+		Movimiento movimiento = new Movimiento(cantidad, "Ingreso", Signo.H);
+		movs.add(movimiento);
+		System.out.println("Ingreso de " + cantidad + "€");
+	}
+
+	public void retirar(double cantidad) {
+		if (saldo - cantidad >= -500) {
+			saldo -= cantidad;
+			Movimiento movimiento = new Movimiento(cantidad, "Retirada", Signo.D);
+			movs.add(movimiento);
+			System.out.println("Retirada de " + cantidad + "€");
+		} else {
+			System.out.println("Fondos insuficientes (" + saldo + "€) en la cuenta " + numero + " para la retirada de "
+					+ cantidad + "€");
+		}
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
 	}
 	
-	public void ingresar(Double i) {
-		this.saldo += i;
+	public String getNumero() {
+		return this.numero;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
 	}
 	
-	public void retirar(Double d)
-	{
-		this.saldo -= d;
+	public String getTitular() {
+		return this.titular;
 	}
+
+	public void setmMovimientos(List<Movimiento> movs) {
+		this.movs = movs;
+	}
+	
+	public List<Movimiento> getmMovimientos() {
+		return this.movs;
+	}
+
 }
